637425bbb48a16bb25dc0e5088099d14904fa9e7
Mahesh Jadhav <106246063+Mahi444555@users.noreply.github.com>
2023-10-10 16:32:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ connection();
 
 //middlewares
 app.use(express.json())
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 
 //routes
